aafaf377131a9cbecb5967fdfaa019b84d4f3de6
cid02259287 <157070040+jy3122@users.noreply.github.com>
2024-11-21 11:02:54 +0000

    Update main.s

--- main.s
+++ main.s
@@ -1,13 +1,13 @@
 #include <xc.inc>
 
 extrn	Keypad_Setup, Keypad_read_input, Keypad_decode  ; external subroutines
-extrn	LCD_Setup, LCD_Write_Message
+extrn	LCD_Setup, LCD_Write_Message, LCD_Send_Byte_D
 	
 psect	udata_acs   ; reserve data space in access ram
 Keypad_result:  ds   1
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
     
-psecpsect main_code, class=CODE
+psect main_code, class=CODE
     
 Main_loop:
     call Keypad_Setup
@@ -15,10 +15,10 @@ Main_loop:
     
 Main_Loop_Start:
     call Keypad_read_input
-    movf Keypad_result, W
+    movf Keypad_result, W, A
     call LCD_Send_Byte_D
     movlw 0xFF
-    movwf delay_count
+    movwf delay_count, A
     call delay
     goto Main_Loop_Start
     
@@ -27,4 +27,4 @@ delay:	decfsz	delay_count, A	; decrement until zero
 	bra	delay
 	return
 
-	end	rst
+	end
